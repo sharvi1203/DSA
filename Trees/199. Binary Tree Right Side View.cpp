22385ea8ccf679fbcf8c390eef85b4d791904143
Sharvi Sethi <43950347+sharvi1203@users.noreply.github.com>
2020-06-28 18:53:32 +0530

    video dekh ke kiya dobara krio

---
+++ Trees/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,53 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+
+//this is extension of level order traversal, isme humei har level ke last element ko dalna hai vector ke  andr issliye hum queue ka use kr rhe hai
+// Second kyunki  hum traveral kr rhe queue ke sath toh ideally isko bfs approach bhi bol skte hai
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> v;
+        if(root==NULL){
+            return v;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* current=q.front();
+                q.pop();
+                if(i==size-1){
+                    v.push_back(current->val);
+                }
+                if(current->left!=NULL){
+                    q.push(current->left);
+                }
+                if(current->right!=NULL){
+                    q.push(current->right);
+                }
+            }
+        }
+        return v;
+    }
+};
