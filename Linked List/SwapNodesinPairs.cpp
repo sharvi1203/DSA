c74cfc9c92e1de26d00b9414785cf8083b4611ef
Sharvi Sethi <43950347+sharvi1203@users.noreply.github.com>
2020-06-30 08:33:28 +0530

    Cheating O(n)

---
+++ Linked List/SwapNodesinPairs.cpp
@@ -0,0 +1,36 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+       if(head==NULL){
+           return head;
+       }
+       if(head->next==NULL){
+            return head;      
+       }
+       ListNode* nxt=head->next;
+       head->next=swapPairs(nxt->next);
+       nxt->next=head;
+       return nxt; 
+    }
+};
