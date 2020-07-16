58430d5e07adced6c1587774a783ca4b0a8231a6
Sharvi Sethi <43950347+sharvi1203@users.noreply.github.com>
2020-07-16 12:49:57 +0530

    Easy peasy(set se bhi hoskta)

---
+++ Leetcode Interview/Find the Duplicate Number.cpp
@@ -0,0 +1,18 @@
+/*
+1. Sort
+2. Set
+*/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i])==s.end()){
+                s.insert(nums[i]);
+            }else{
+                return nums[i];    
+            }
+        }
+        return -1;
+    }
+};
